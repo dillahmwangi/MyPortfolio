97a31b5b257fcd878b50e5b0001fb7a551d875f3
dillahmwangi <dillahmwangi@gmail.com>
2025-06-09 20:23:05 +0300

    created a footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-900 text-white text-center'>
+        This is my footer
+
+    </footer>
+  )
+}
+
+export default Footer;
